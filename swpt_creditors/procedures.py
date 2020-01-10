fb68b6d2e85caca0fc410cdc5cfa77aa94ccae36
Evgeni Pandurski <epandurski@gmail.com>
2020-01-10 18:18:03 +0200

    Add process_committed_transfer_signal() implementation

--- swpt_creditors/procedures.py
+++ swpt_creditors/procedures.py
@@ -4,8 +4,9 @@ from typing import TypeVar, Optional, Callable, Tuple, List
 from flask import current_app
 from sqlalchemy.exc import IntegrityError
 from .extensions import db
-from .models import CommittedTransfer, PendingCommittedTransfer, InitiatedTransfer, RunningTransfer, \
-    MIN_INT64, MAX_INT64, ROOT_CREDITOR_ID
+from .models import AccountLedger, LedgerAddition, CommittedTransfer, PendingCommittedTransfer, \
+    InitiatedTransfer, RunningTransfer, \
+    MIN_INT64, MAX_INT64, ROOT_CREDITOR_ID, date_to_int24
 
 T = TypeVar('T')
 atomic: Callable[[T], T] = db.atomic
@@ -31,4 +32,54 @@ def process_committed_transfer_signal(
     assert -MAX_INT64 <= committed_amount <= MAX_INT64
     assert -MAX_INT64 <= new_account_principal <= MAX_INT64
 
-    # TODO: Add implementation.
+    db.session.add(CommittedTransfer(
+        creditor_id=creditor_id,
+        debtor_id=debtor_id,
+        transfer_seqnum=transfer_seqnum,
+        transfer_epoch=transfer_epoch,
+        coordinator_type=coordinator_type,
+        other_creditor_id=other_creditor_id,
+        committed_at_ts=committed_at_ts,
+        committed_amount=committed_amount,
+        transfer_info=transfer_info,
+        new_account_principal=new_account_principal,
+    ))
+    try:
+        db.session.flush()
+    except IntegrityError:
+        # Normally, this can happen only when the committed transfer
+        # message has been re-delivered. Therefore, no action should
+        # be taken.
+        db.session.rollback()
+        return
+
+    ledger = _get_or_create_ledger(debtor_id, creditor_id)
+    if transfer_epoch > ledger.epoch:
+        # A new "epoch" has started -- the old ledger must be
+        # discarded, and a brand new ledger created.
+        ledger.epoch = transfer_epoch
+        ledger.principal = 0
+        ledger.first_transfer_seqnum = (date_to_int24(transfer_epoch) << 40) + 1
+        ledger.next_transfer_seqnum = ledger.first_transfer_seqnum
+
+    db.session.add(PendingCommittedTransfer(
+        creditor_id=creditor_id,
+        debtor_id=debtor_id,
+        transfer_seqnum=transfer_seqnum,
+        committed_at_ts=committed_at_ts,
+    ))
+
+
+def _create_ledger(debtor_id: int, creditor_id: int) -> AccountLedger:
+    ledger = AccountLedger(debtor_id=debtor_id, creditor_id=creditor_id)
+    with db.retry_on_integrity_error():
+        db.session.add(ledger)
+    return ledger
+
+
+def _get_or_create_ledger(debtor_id: int, creditor_id: int, lock: bool = False) -> AccountLedger:
+    if lock:
+        ledger = AccountLedger.lock_instance((debtor_id, creditor_id))
+    else:
+        ledger = AccountLedger.get_instance((debtor_id, creditor_id))
+    return ledger or _create_ledger(debtor_id, creditor_id)
