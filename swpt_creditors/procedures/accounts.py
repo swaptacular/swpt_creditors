bb6f4fbc1799b1173cf77f4e5b930560a3030dc2
Evgeni Pandurski <epandurski@gmail.com>
2020-08-29 00:45:23 +0300

    Move process_account_transfer_signal() to transfers.py

--- swpt_creditors/procedures/accounts.py
+++ swpt_creditors/procedures/accounts.py
@@ -1,4 +1,4 @@
-from datetime import datetime, date, timedelta, timezone
+from datetime import datetime, date, timezone
 from typing import TypeVar, Callable, Tuple, List, Optional
 from sqlalchemy.sql.expression import func
 from sqlalchemy.orm import joinedload, exc, load_only, Load
@@ -437,99 +437,22 @@ def process_account_update_signal(
 
     if new_server_account:
         _insert_ledger_entry(data, 0, 0, 0, data.ledger_last_transfer_committed_at_ts, current_ts)
-        _ensure_pending_ledger_update(data.creditor_id, data.debtor_id)
+        ensure_pending_ledger_update(data.creditor_id, data.debtor_id)
 
 
 @atomic
-def process_account_transfer_signal(
-        debtor_id: int,
-        creditor_id: int,
-        creation_date: date,
-        transfer_number: int,
-        coordinator_type: str,
-        sender: str,
-        recipient: str,
-        acquired_amount: int,
-        transfer_note: str,
-        committed_at_ts: datetime,
-        principal: int,
-        ts: datetime,
-        previous_transfer_number: int,
-        retention_interval: timedelta) -> None:
-
-    assert MIN_INT64 <= debtor_id <= MAX_INT64
+def ensure_pending_ledger_update(creditor_id: int, debtor_id: int) -> None:
     assert MIN_INT64 <= creditor_id <= MAX_INT64
-    assert 0 < transfer_number <= MAX_INT64
-    assert acquired_amount != 0
-    assert MIN_INT64 <= acquired_amount <= MAX_INT64
-    assert MIN_INT64 <= principal <= MAX_INT64
-    assert 0 <= previous_transfer_number <= MAX_INT64
-    assert previous_transfer_number < transfer_number
+    assert MIN_INT64 <= debtor_id <= MAX_INT64
 
-    current_ts = datetime.now(tz=timezone.utc)
-    if (current_ts - min(ts, committed_at_ts)) > retention_interval:
-        return
-
-    committed_transfer_query = CommittedTransfer.query.filter_by(
-        debtor_id=debtor_id,
-        creditor_id=creditor_id,
-        creation_date=creation_date,
-        transfer_number=transfer_number,
-    )
-    if db.session.query(committed_transfer_query.exists()).scalar():
-        return
-
-    # NOTE: We must obtain a "FOR SHARE" lock here to ensure that the
-    # `ledger_last_transfer_number` will not be increased by another
-    # concurrent transaction, without inserting a corresponding
-    # `PendingLedgerUpdate` record, which would result in the ledger
-    # not being updated.
-    ledger_data_query = db.session.\
-        query(AccountData.creation_date, AccountData.ledger_last_transfer_number).\
-        filter_by(creditor_id=creditor_id, debtor_id=debtor_id).\
-        with_for_update(read=True)
-    try:
-        ledger_date, ledger_last_transfer_number = ledger_data_query.one()
-    except exc.NoResultFound:
-        return
-
-    with db.retry_on_integrity_error():
-        db.session.add(CommittedTransfer(
-            debtor_id=debtor_id,
-            creditor_id=creditor_id,
-            creation_date=creation_date,
-            transfer_number=transfer_number,
-            coordinator_type=coordinator_type,
-            sender_id=sender,
-            recipient_id=recipient,
-            acquired_amount=acquired_amount,
-            transfer_note=transfer_note,
-            committed_at_ts=committed_at_ts,
-            principal=principal,
-            previous_transfer_number=previous_transfer_number,
-        ))
-
-    paths, types = get_paths_and_types()
-    db.session.add(PendingLogEntry(
-        creditor_id=creditor_id,
-        added_at_ts=current_ts,
-        object_type=types.committed_transfer,
-        object_uri=paths.committed_transfer(
-            creditorId=creditor_id,
-            debtorId=debtor_id,
-            creationDate=creation_date,
-            transferNumber=transfer_number,
-        ),
-    ))
-
-    if creation_date == ledger_date and previous_transfer_number == ledger_last_transfer_number:
-        _ensure_pending_ledger_update(creditor_id, debtor_id)
+    if PendingLedgerUpdate.get_instance((creditor_id, debtor_id)) is None:
+        with db.retry_on_integrity_error():
+            db.session.add(PendingLedgerUpdate(creditor_id=creditor_id, debtor_id=debtor_id))
 
 
 @atomic
 def get_pending_ledger_updates(max_count: int = None) -> List[Tuple[int, int]]:
     query = db.session.query(PendingLedgerUpdate.creditor_id, PendingLedgerUpdate.debtor_id)
-
     if max_count is not None:
         query = query.limit(max_count)
 
@@ -538,10 +461,9 @@ def get_pending_ledger_updates(max_count: int = None) -> List[Tuple[int, int]]:
 
 @atomic
 def process_pending_ledger_update(creditor_id: int, debtor_id: int, max_count: int = None) -> bool:
-    """Return `False` if some legible committed transfers remained unprocessed."""
+    """Returns `False` if some legible committed transfers remained unprocessed."""
 
     current_ts = datetime.now(tz=timezone.utc)
-
     query = db.session.\
         query(PendingLedgerUpdate, AccountData).\
         join(PendingLedgerUpdate.account_data).\
@@ -623,15 +545,6 @@ def _discard_orphaned_account(creditor_id: int, debtor_id: int, config_flags: in
             config_flags=DEFAULT_CONFIG_FLAGS | scheduled_for_deletion_flag,
             config='',
         ))
-
-
-def _ensure_pending_ledger_update(creditor_id: int, debtor_id: int) -> None:
-    assert MIN_INT64 <= creditor_id <= MAX_INT64
-    assert MIN_INT64 <= debtor_id <= MAX_INT64
-
-    if PendingLedgerUpdate.get_instance((creditor_id, debtor_id)) is None:
-        with db.retry_on_integrity_error():
-            db.session.add(PendingLedgerUpdate(creditor_id=creditor_id, debtor_id=debtor_id))
 
 
 def _insert_ledger_entry(
--- swpt_creditors/procedures/transfers.py
+++ swpt_creditors/procedures/transfers.py
@@ -1,11 +1,103 @@
 from uuid import UUID
-from datetime import datetime, timezone
+from datetime import datetime, timezone, date, timedelta
 from typing import TypeVar, Callable, Optional
+from sqlalchemy.orm import exc
 from swpt_creditors.extensions import db
-from swpt_creditors.models import DirectTransfer, RunningTransfer, MIN_INT64, MAX_INT64
+from swpt_creditors.models import (
+    AccountData, PendingLogEntry, DirectTransfer, RunningTransfer, CommittedTransfer,
+    MIN_INT64, MAX_INT64,
+)
+from .common import get_paths_and_types
+from .accounts import ensure_pending_ledger_update
 
 T = TypeVar('T')
 atomic: Callable[[T], T] = db.atomic
+
+
+@atomic
+def process_account_transfer_signal(
+        debtor_id: int,
+        creditor_id: int,
+        creation_date: date,
+        transfer_number: int,
+        coordinator_type: str,
+        sender: str,
+        recipient: str,
+        acquired_amount: int,
+        transfer_note: str,
+        committed_at_ts: datetime,
+        principal: int,
+        ts: datetime,
+        previous_transfer_number: int,
+        retention_interval: timedelta) -> None:
+
+    assert MIN_INT64 <= debtor_id <= MAX_INT64
+    assert MIN_INT64 <= creditor_id <= MAX_INT64
+    assert 0 < transfer_number <= MAX_INT64
+    assert acquired_amount != 0
+    assert MIN_INT64 <= acquired_amount <= MAX_INT64
+    assert MIN_INT64 <= principal <= MAX_INT64
+    assert 0 <= previous_transfer_number <= MAX_INT64
+    assert previous_transfer_number < transfer_number
+
+    current_ts = datetime.now(tz=timezone.utc)
+    if (current_ts - min(ts, committed_at_ts)) > retention_interval:
+        return
+
+    committed_transfer_query = CommittedTransfer.query.filter_by(
+        debtor_id=debtor_id,
+        creditor_id=creditor_id,
+        creation_date=creation_date,
+        transfer_number=transfer_number,
+    )
+    if db.session.query(committed_transfer_query.exists()).scalar():
+        return
+
+    # NOTE: We must obtain a "FOR SHARE" lock here to ensure that the
+    # `ledger_last_transfer_number` will not be increased by another
+    # concurrent transaction, without inserting a corresponding
+    # `PendingLedgerUpdate` record, which would result in the ledger
+    # not being updated.
+    ledger_data_query = db.session.\
+        query(AccountData.creation_date, AccountData.ledger_last_transfer_number).\
+        filter_by(creditor_id=creditor_id, debtor_id=debtor_id).\
+        with_for_update(read=True)
+    try:
+        ledger_date, ledger_last_transfer_number = ledger_data_query.one()
+    except exc.NoResultFound:
+        return
+
+    with db.retry_on_integrity_error():
+        db.session.add(CommittedTransfer(
+            debtor_id=debtor_id,
+            creditor_id=creditor_id,
+            creation_date=creation_date,
+            transfer_number=transfer_number,
+            coordinator_type=coordinator_type,
+            sender_id=sender,
+            recipient_id=recipient,
+            acquired_amount=acquired_amount,
+            transfer_note=transfer_note,
+            committed_at_ts=committed_at_ts,
+            principal=principal,
+            previous_transfer_number=previous_transfer_number,
+        ))
+
+    paths, types = get_paths_and_types()
+    db.session.add(PendingLogEntry(
+        creditor_id=creditor_id,
+        added_at_ts=current_ts,
+        object_type=types.committed_transfer,
+        object_uri=paths.committed_transfer(
+            creditorId=creditor_id,
+            debtorId=debtor_id,
+            creationDate=creation_date,
+            transferNumber=transfer_number,
+        ),
+    ))
+
+    if creation_date == ledger_date and previous_transfer_number == ledger_last_transfer_number:
+        ensure_pending_ledger_update(creditor_id, debtor_id)
 
 
 @atomic
