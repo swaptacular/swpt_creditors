4b8063d8d0b388285fc615db99b7d6fc0020225c
Evgeni Pandurski <epandurski@gmail.com>
2021-02-16 20:51:42 +0200

    Exclude tests from the image, fix actors mock

--- tests/test_actors.py
+++ tests/test_actors.py
@@ -4,15 +4,13 @@ D_ID = -1
 C_ID = 4294967296
 
 
-@pytest.fixture(scope='module')
-def actors():
-    from unittest import mock
-
+@pytest.fixture(scope='function')
+def actors(mocker):
     class Broker:
-        def actor(self, f, **kw):
-            return f
+        def actor(self, fn=None, **kw):
+            return fn or (lambda x: x)
 
-    mock.patch('swpt_accounts.extensions.protocol_broker', Broker())
+    mocker.patch('swpt_creditors.extensions.protocol_broker', Broker())
     from swpt_creditors import actors
     return actors
 
