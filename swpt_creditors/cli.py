f41e58de3f7d402944eedbc697e5cd5d4df23f9f
Evgeni Pandurski <epandurski@gmail.com>
2021-02-18 16:40:22 +0200

    Improve todo

--- swpt_creditors/cli.py
+++ swpt_creditors/cli.py
@@ -173,13 +173,13 @@ def process_log_additions(threads, wait, quit_early):
     """
 
     # TODO: Consider allowing load-sharing between multiple
-    #       processors. A possible way to do this is to separate the
+    #       containers. A possible way to do this is to separate the
     #       `args collection` in multiple buckets, assigning a
-    #       dedicated `ThreadPoolProcessor`-process for each bucket.
-    #       This is also true for the other "process_*" CLI
-    #       commands. Note that this would makes sense only if the
-    #       load is CPU-bound, which is unlikely if we implement the
-    #       logic in stored procedures.
+    #       dedicated container for each bucket. This may also be true
+    #       for the other "process_*" CLI commands. Note that this
+    #       would makes sense only if the load is CPU-bound, which is
+    #       unlikely if we re-implement the logic in stored
+    #       procedures.
 
     threads = threads or current_app.config['APP_PROCESS_LOG_ADDITIONS_THREADS']
     wait = wait if wait is not None else current_app.config['APP_PROCESS_LOG_ADDITIONS_WAIT']
